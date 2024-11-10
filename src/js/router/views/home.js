c3f267df1e07555478bad8f90131e42032613bb3
AdrianAbusland <Adrian.Abusland@gmail.com>
2024-11-10 18:04:57 +0100

    Error fixing

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,7 +1,7 @@
-import { displayPosts } from "../../api/post/postsStructure";
-import { readPosts } from "../../api/post/read";
-import { setLogoutListener } from "../../ui/global/logout";
-import { authGuard } from "../../utilities/authGuard";
+import { displayPosts } from "../../api/post/postsStructure.js";
+import { readPosts } from "../../api/post/read.js";
+import { setLogoutListener } from "../../ui/global/logout.js";
+import { authGuard } from "../../utilities/authGuard.js";
 
 async function runPage() {
     const posts = await readPosts()
