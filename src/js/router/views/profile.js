6db3c5830e01ce4cba5a3f4190a5e7021f3c8e80
AdrianAbusland <Adrian.Abusland@gmail.com>
2024-11-10 18:38:27 +0100

    Error fixing

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,4 +1,4 @@
-// import { authGuard } from "../../utilities/authGuard";
+import { authGuard } from "../../utilities/authGuard";
 // import { readPostsByUser } from "../../api/post/read";
 // import { displayPosts } from "../../api/post/postsStructure";
 // import { setLogoutListener } from "../../ui/global/logout";
